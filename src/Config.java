5ce895b4f53d7bfc9a7852ffd6d66aba66b4de12
Alejololer <acarlos29112003@gmail.com>
2024-05-07 23:21:41 -0500

    commit

--- src/Config.java
+++ src/Config.java
@@ -11,14 +11,14 @@ import org.jgap.IChromosome;
 public class Config {
     public void configurarAG(int población, int longitud, int numero_evoluciones, int numero_iteraciones) throws InvalidConfigurationException{
         Configuration conf = new DefaultConfiguration();
-        FuncionAptitud fa = new FuncionAptitud();
+        FuncionAptitud fa = new FuncionAptitud(longitud);
         try {
             conf.setFitnessFunction(fa);
             conf.setPopulationSize(población);
             IChromosome c = new Chromosome(conf, ejemploChromosome(conf, población));
             conf.setSampleChromosome(c);
             Genotype poblaciGenotype = Genotype.randomInitialGenotype(conf);
-            evolucionar(poblaciGenotype, numero_evoluciones, numero_iteraciones);
+            evolucionar(poblaciGenotype, numero_evoluciones, numero_iteraciones, longitud);
         } catch (InvalidConfigurationException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -38,59 +38,41 @@ public class Config {
         return ejemplo;
     }
 
-    private void evolucionar(Genotype población, int número_evoluciones, int número_iteraciones){
+    private void evolucionar(Genotype población, int número_evoluciones, int número_iteraciones, int longitud){
         for (int i=0; i < número_iteraciones; i++){
             población.evolve(número_evoluciones);
             población.getFittestChromosome(); //ón.getFitt(número_iteraciones); //
-            ImprimirMejores(población.getChromosomes());
+            ImprimirMejores(población.getChromosomes(), longitud);
             System.out.println("--------------------------------");
-            ImprimirMejor(población.getFittestChromosome());
+            ImprimirMejor(población.getFittestChromosome(), longitud);
             System.out.println("--------------------------------");
         }
     }
 
-    private void ImprimirMejores(IChromosome[] ic){
+    private void ImprimirMejores(IChromosome[] ic, int longitud){
         for (IChromosome iChromosome : ic) {
-            ImprimirMejor(iChromosome);
+            ImprimirMejor(iChromosome, longitud);
         }
     }
 
-    private void ImprimirMejor(IChromosome ic){
-        Integer v0 = (Integer) ic.getGene(0).getAllele();
-        Integer v1 = (Integer) ic.getGene(1).getAllele();
-        Integer v2 = (Integer) ic.getGene(2).getAllele();
-        Integer v3 = (Integer) ic.getGene(3).getAllele();
-        Integer v4 = (Integer) ic.getGene(4).getAllele();
-        Integer v5 = (Integer) ic.getGene(5).getAllele();
-        Integer v6 = (Integer) ic.getGene(6).getAllele();
-        Integer v7 = (Integer) ic.getGene(7).getAllele();
-        Integer v8 = (Integer) ic.getGene(8).getAllele();
-        Integer v9 = (Integer) ic.getGene(9).getAllele();
-        Integer v10 = (Integer) ic.getGene(10).getAllele();
-        Integer v11 = (Integer) ic.getGene(11).getAllele();
-        Integer v12 = (Integer) ic.getGene(12).getAllele();
-        Integer v13 = (Integer) ic.getGene(13).getAllele();
-        Integer v14 = (Integer) ic.getGene(14).getAllele();
-        Integer v15 = (Integer) ic.getGene(15).getAllele();
-        String x = v1.toString() + v2.toString() + v3.toString() + v4.toString();
-        String y = v6.toString() + v7.toString() + v8.toString() + v9.toString();
-        String z = v11.toString() + v12.toString() + v13.toString() + v14.toString();
-        int vx = Integer.parseInt(x,2);
-        int vy = Integer.parseInt(y,2);
-        int vz = Integer.parseInt(z,2);
-        if(v0==0){
-            vx = -vx;
+    private void ImprimirMejor(IChromosome ic, int longitud) {
+        String x ="";
+        String d = "";
+        for (int i = 1; i < longitud; i++){
+            if(i > longitud/2){
+                d = d + (Integer) ic.getGene(i).getAllele();
+            }
+            x = x + (Integer) ic.getGene(i).getAllele();
         }
-        if(v5==0){
-            vy = -vy;
+        int v0 = (int) ic.getGene(0).getAllele();
+        int vx = Integer.parseInt(x, 2);
+        double vd = Integer.parseInt(d, 2);
+        double valor = vx + vd;
+
+        if(v0 == 0){
+            valor= -valor;
         }
-        if(v10==0){
-            vz = -vz;
-        }
-        double score =(vx*vx)+(vy*vy)+(vz*vz);
-        if(score<0){
-            score=0;
-        }
-        System.out.println(vx+" "+vy+" "+vz);
+        System.out.println("Valor de x: " + valor);
+        System.out.println("    Valor de la función: "+ Math.log10(Math.sqrt(Math.pow(valor, 4))));
     }
 }
--- src/FuncionAptitud.java
+++ src/FuncionAptitud.java
@@ -3,44 +3,38 @@ import org.jgap.IChromosome;
 
 public class FuncionAptitud extends FitnessFunction{
 
+    private int longitud;
+
+    
+    public FuncionAptitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+
     @Override
     protected double evaluate(IChromosome ic) {
-        Integer v0 = (Integer) ic.getGene(0).getAllele();
-        Integer v1 = (Integer) ic.getGene(1).getAllele();
-        Integer v2 = (Integer) ic.getGene(2).getAllele();
-        Integer v3 = (Integer) ic.getGene(3).getAllele();
-        Integer v4 = (Integer) ic.getGene(4).getAllele();
-        Integer v5 = (Integer) ic.getGene(5).getAllele();
-        Integer v6 = (Integer) ic.getGene(6).getAllele();
-        Integer v7 = (Integer) ic.getGene(7).getAllele();
-        Integer v8 = (Integer) ic.getGene(8).getAllele();
-        Integer v9 = (Integer) ic.getGene(9).getAllele();
-        Integer v10 = (Integer) ic.getGene(10).getAllele();
-        Integer v11 = (Integer) ic.getGene(11).getAllele();
-        Integer v12 = (Integer) ic.getGene(12).getAllele();
-        Integer v13 = (Integer) ic.getGene(13).getAllele();
-        Integer v14 = (Integer) ic.getGene(14).getAllele();
-        Integer v15 = (Integer) ic.getGene(15).getAllele();
-        String x = v1.toString() + v2.toString() + v3.toString() + v4.toString();
-        String y = v6.toString() + v7.toString() + v8.toString() + v9.toString();
-        String z = v11.toString() + v12.toString() + v13.toString() + v14.toString();
-        int vx = Integer.parseInt(x,2);
-        int vy = Integer.parseInt(y,2);
-        int vz = Integer.parseInt(z,2);
-        if(v0==0){
-            vx = -vx;
+        String x ="";
+        String d = "";
+        for (int i = 1; i < longitud; i++){
+            if(i > longitud/2){
+                d = d + (Integer) ic.getGene(i).getAllele();
+            }
+            x = x + (Integer) ic.getGene(i).getAllele();
         }
-        if(v5==0){
-            vy = -vy;
+        int v0 = (int) ic.getGene(0).getAllele();
+        int vx = Integer.parseInt(x, 2);
+        double vd = Integer.parseInt(d, 2);
+        double valor = vx + vd;
+
+        if(v0 == 0){
+            valor= -valor;
         }
-        if(v10==0){
-            vz = -vz;
-        }
-        double score =675-(vx*vx)+(vy*vy)+(vz*vz);
-        if(score<0){
-            score=0;
-        }
-        System.out.println(vx+" "+vy+" "+vz);
+
+        int valMaxGen = (int) ( Math.pow(2, (longitud-1)/2)+(1/Math.pow(2, (longitud-1))/2)-Math.pow(2, -1)); //Para el máximo ya no me acordaba esta formula para las cadenas de bits lel
+        double valMax = Math.log10(Math.sqrt(Math.pow(valMaxGen, 4)));
+        double score = valMax - Math.log10(Math.sqrt(Math.pow(valor, 4)));
+        if (score < 0)
+            score = 0;
         return score;
     }
     
